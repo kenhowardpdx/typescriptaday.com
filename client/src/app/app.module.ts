8eb0617350c41c23712133b0210942227eedabb8
Ken Howard <ken@simplicatedweb.com>
2017-04-30 20:57:59 -0700

    refactor(Toolbar): Add toolbar to main layout
    * Adds Angular Material
    * Adds Flex Layout

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdToolbarModule, MdButtonModule, MdIconModule, MdCardModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+const MaterialModules = [ MdToolbarModule, MdButtonModule, MdIconModule, MdCardModule ];
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,6 +23,9 @@ import { NotFoundComponent } from './not-found/not-found.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    BrowserAnimationsModule,
+    MaterialModules,
+    FlexLayoutModule,
     AppRoutingModule
   ],
   providers: [],
