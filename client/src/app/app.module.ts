c86258c317eb9205d68731e8df6b8a440afe8d3b
Ken Howard <ken@simplicatedweb.com>
2017-05-10 18:40:28 -0700

    Add login form

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdToolbarModule, MdButtonModule, MdIconModule, MdCardModule } from '@angular/material';
+import { MaterialModule} from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
-const MaterialModules = [ MdToolbarModule, MdButtonModule, MdIconModule, MdCardModule ];
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,7 +27,7 @@ import { AuthGuard, AuthService } from './services';
     FormsModule,
     HttpModule,
     BrowserAnimationsModule,
-    MaterialModules,
+    MaterialModule,
     FlexLayoutModule,
     AppRoutingModule
   ],
